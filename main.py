824316a3eb8290f664443a6fe25ec7349b8abe0d
gjgfuj <gjgfuj@gjgfuj-eME642.(none)>
2013-08-11 22:32:50 +1000

    Added more to main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pygame
 pygame.init()
-import pykemon
+#import pykemon
 
-display = pygame.display.set_mode(())
+display = pygame.display.set_mode((256,194*2))
+topscreen = pygame.Surface((256, 192))
+bottomscreen = pygame.Surface((256, 192))
+
+while True:
+	topscreen.fill((255, 0, 255))
+	bottomscreen.fill((0, 255, 255))
+	
+	
+	display.blit(topscreen, (0, 0))
+	display.blit(bottomscreen, (0, 196))
+	pygame.display.flip()
